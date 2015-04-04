28068cf30e174ea5e730f816df94b95baa761b89
Spence Southard <Spence.Southard@gmail.com>
2015-04-04 11:55:49 -0400

    Added assets folder

--- pacman-style/gamemap.js
+++ pacman-style/gamemap.js
@@ -2,3 +2,13 @@
  * Created by Spence on 4/4/2015.
  */
 
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
