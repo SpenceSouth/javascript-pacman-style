727e6123a3d6868d5992b55aff91a2aa238f2c10
Marcus <marcuschagans@gmail.com>
2015-04-10 18:24:49 -0400

    Testing mysql connection

--- pacman-style/ScoresCall.php
+++ pacman-style/ScoresCall.php
@@ -4,14 +4,12 @@ $username = "n00858385";
 $password = "cop8385";
 
 echo "<p>Inside php File</p>";
-try{
-	$conn = new PDO("mysql:host = 139.62.63.234; dbname= n00858385", $username, $password);
-	//set the PDO error mode to exception
-	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	echo "<p>Connected successfully</p>";
-}
-catch(PDOException $e){
-	echo "<p>Connection failed: " . $e->getMessage() . "</p>";
-}
-$conn = null;
+
+	$conn =mysql_connect($servername, $username, $password);
+
+	//Check Connection
+    if (!$conn) {
+        die("Connection failed:" . mysqli_connect_error());
+    }
+    echo "<p>Connected successfully</p>";
 ?>
